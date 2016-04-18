dc7a19295fd02927f068578a8177a8fda27cb929
Gavin Sidebottom <gwsidebottom@gmail.com>
2016-04-18 19:52:16 -0400

    adding documentation

--- database/__init__.py
+++ database/__init__.py
@@ -2,6 +2,16 @@ from __future__ import print_function
 
 
 class InMemoryDatabase(object):
+    """Stores key-value pairs and maintains an index of current value counts in memory
+
+    Attributes:
+        data: Internal storage for key-value pairs. Values are lists which act as a 
+            value's history. Only one value can be 'active' in these lists.
+        value_index: Dictionary which maps each active value in the data to the number
+            of times it appears. This is maintained to reduce lookup times when 
+            determining the number of keys that have a certain value.
+    """
+
     def __init__(self):
         self.data = {}
         self.value_index = {}
@@ -24,7 +34,7 @@ class InMemoryDatabase(object):
     def add_or_replace(self, var, value, new=True):
         """
         Adds a new value which will act as the current value at key 'var' or
-        replaces the current value. Updates value_index accordingly.
+        replaces the current value. Updates the value index accordingly.
         """
         if new and not self.is_set(var):
             self.data[var] = []
@@ -58,6 +68,10 @@ class InMemoryDatabase(object):
         self.decrement_value_index(value)
     
     def flatten(self):
+        """
+        Sets each value list in the data to contain only the active value, effectively
+        deleting the 'history' of each value list.
+        """
         for var in self.data.keys():
             self.data[var] = [self.data[var][0]]
             
@@ -69,14 +83,23 @@ class InMemoryDatabase(object):
 
     
 class DbSession(object):
+    """Stores key-value pairs and maintains an index of current value counts in memory
+
+    Attributes:
+        data: Internal storage for key-value pairs. Values are lists which act as a 
+            value's history. Only one value can be 'active' in these lists.
+        value_index: Dictionary which maps each active value in the data to the number
+            of times it appears. This is maintained to reduce lookup times when 
+            determining the number of keys that have a certain value.
+    """
     def __init__(self):
         self.database = InMemoryDatabase()
         self.transaction_stack = []
         self.reset_transaction_state()
     
     def reset_transaction_state(self):
-        # Transaction stack will always have a 'base' transaction - cannot be rolled back/commited
         self.current_trans = set() if not self.transaction_stack else self.transaction_stack[0]
+        # Transaction stack will always have a 'base' transaction - cannot be rolled back/commited
         self.transaction_stack = [self.current_trans]
         
     def pop_transaction(self):
